98b84f808dcd5a8602be2f99918eadf664b312dd
itily <ilyha_roza@mail.ru>
2021-11-22 16:36:57 +0300

    Initial commit

---
+++ config/sidebar.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itily
+ * Date: 20.11.2021
+ * Time: 21:06
+ */
+
+return [
+    "items" => [
+        [
+            "label" => Yii::t('app', 'Главная'),
+            "url" => "/",
+            "icon" => "home",
+        ],
+    ],
+];
