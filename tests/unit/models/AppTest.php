82724d52f4cc63d7a34e868574069ae618b4bb59
itily <ilyha_roza@mail.ru>
2021-11-25 11:50:36 +0300

    Check actions

---
+++ tests/unit/models/AppTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: itily
+ * Date: 25.11.2021
+ * Time: 10:41
+ */
+
+class AppTest extends \Codeception\Test\Unit
+{
+    public function testFindUserById()
+    {
+        expect_that(true);
+    }
+}
