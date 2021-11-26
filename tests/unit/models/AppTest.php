aa7f3f3f93340df176aeb09dbd3cd48ac4008217
itily <ilyha_roza@mail.ru>
2021-11-26 16:24:43 +0300

    12345 - 7

--- tests/unit/models/AppTest.php
+++ tests/unit/models/AppTest.php
@@ -28,4 +28,18 @@ class AppTest extends \Codeception\Test\Unit
         expect_that(true);
     }
 
+    public function testFindUser4()
+    {
+        expect_that(true);
+    }
+
+    public function testFindUser5()
+    {
+        expect_that(true);
+    }
+
+    public function testFindUser6()
+    {
+        expect_that(true);
+    }
 }
